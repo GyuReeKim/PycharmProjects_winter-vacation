ff19b375b92abbefbc499356fbf04759a87a0c65
GyuReeKim <starkim96@naver.com>
2019-12-12 11:40:44 +0900

    치즈 푸는중

---
+++ 191211/치즈.py
@@ -0,0 +1,9 @@
+# 백준 2636번
+
+# 각 탐색에서 녹은 치즈의 개수 확인 필요
+# 탐색에서 녹은 치즈의 개수가 0이면 중단
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+melt = 1
+while melt != 0:
+    pass
